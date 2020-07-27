8ab7a3ad0b47f67c2fca7c4f635e527f73f5c6e5
loiola0 <victorloiola@alu.ufc.br>
2020-07-27 16:13:37 -0300

    40% do cursinho feito

---
+++ huntapp/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';
+import Header from './components/Header';
+
+const App = () =>(
+
+    <div className="App">
+
+        <Header/>
+
+    </div>
+
+);
+
+
+export default App;
